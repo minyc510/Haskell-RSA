12e6eda3411d3795fd67d403589a029d10228254
minyc510 <minyc510@gmail.com>
2018-08-11 23:54:56 -0700

    User input message as executable argument

--- RSA.hs
+++ RSA.hs
@@ -2,43 +2,40 @@
 import System.Random 
 import ModularOperations 
 import Primes             
+import System.Environment
 
 main :: IO()
-main = do
+main = do x <- getArgs
     --Initialize Seeds
-    seed <- newStdGen
-    seedP <- newStdGen
-    seedQ <- newStdGen
+          seedP <- newStdGen
+          seedQ <- newStdGen
 
     --Generate/Calcuate: p,q,n,e,totient, and d
-    let p = randomPrime seedP $ rInteger $ "1024"
-    let q = randomPrime seedQ $ rInteger $ "1024"
-    let n = p*q
-    let e = 65537
-    let totient = lcm (p-1) (q-1)
-    let d = if modMulInv e totient > 0
-            then modMulInv e totient
-            else (modMulInv e totient) + totient
+          let p = randomPrime seedP $ rInteger $ "1024"
+          let q = randomPrime seedQ $ rInteger $ "1024"
+          let n = p*q
+          let e = 65537
+          let totient = lcm (p-1) (q-1)
+          let d = if modMulInv e totient > 0
+                  then modMulInv e totient
+                  else (modMulInv e totient) + totient
 
-    print $ isPrime seed $ p
-    print $ isPrime seed $ q
+          --print $ isPrime seedP $ p
+          --print $ isPrime seedQ $ q
 
-    print $ "p: " ++ show p
-    print $ "q: " ++ show q
-    print $ "n: " ++ show n
-    print $ "d: " ++ show d
+          print $ "p: " ++ show p
+          print $ "q: " ++ show q
+          print $ "n: " ++ show n
+          print $ "d: " ++ show d
 
-    --User Input
-    print $ "Input message:"
-    input <- getLine
-    let pT = read input
-    print $ "plain text: " ++ show pT
+          let pT = read $ head x
+          print $ "plain text: " ++ show pT
 
-    --Encrypt, Decrypt
-    let cT = encrypt pT e n
-    let rT = decrypt cT d n    
-    print $ "cipher text: " ++ show cT
-    print $ "recovered text: " ++ show rT     
+          --Encrypt, Decrypt
+          let cT = encrypt pT e n
+          let rT = decrypt cT d n    
+          print $ "cipher text: " ++ show cT
+          print $ "recovered text: " ++ show rT     
 
     
 rInteger :: String -> Integer
