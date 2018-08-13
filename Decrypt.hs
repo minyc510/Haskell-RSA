ac023302e8bbf9e5fdd1c7622556554b09fc378b
minyc510 <minyc510@gmail.com>
2018-08-13 10:33:19 -0700

    Added Decrypt.hs

---
+++ Decrypt.hs
@@ -0,0 +1,22 @@
+--To compile this file: `ghc -o dec Decrypt.hs`
+import System.Random 
+import ModularOperations 
+import Primes             
+import System.Environment
+
+main :: IO()
+main = do (cipher:privateKey:modulus:_) <- getArgs
+          --Parse Input
+          let c = rInteger $ cipher
+          let d = rInteger $ privateKey
+          let n = rInteger $ modulus
+
+          print $ decrypt c d n
+          
+--Decrypt c (ciphertext) given the private exponent d and modulus n 
+decrypt :: Integer -> Integer -> Integer -> Integer
+decrypt c d n = modExp c d n
+
+rInteger :: String -> Integer
+rInteger = read 
+
