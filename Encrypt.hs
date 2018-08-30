ec2f77c2e743c113c3f825f81710140e7eb6739a
minyc510 <minyc510@gmail.com>
2018-08-30 11:03:42 -0700

    Compileable

--- Encrypt.hs
+++ Encrypt.hs
@@ -1,25 +1,13 @@
 --To compile this file: `ghc -o enc Encrypt.hs`
+import System.Environment
 import Primes
 import ModularOperations
-
-import System.Environment
 import Data.ByteString.Base64
 import Data.ByteString.Conversion
 import Data.ByteString.Internal
+
 main :: IO()
-main = do (message:exponent:modulus64:_) <- getArgs
-          let modulusBS = toByteString (modulus64 :: String)
-          print $ modulusBS
-          let modulus = case decode modulusBS of
-                          Left err -> err
-                          Right msg -> fromByteString msg
-          print $ modulus
-{-
-          --let message = 
-          --let temp = case decode (toByteString (messageB64 :: String)) of
-          --             Left err -> err 
-          --             Right msg -> msg
-          --print $ message
+main = do (message:exponent:modulus:_) <- getArgs
           let m = rInteger $ message
           let e = 65537
           let n = rInteger $ modulus -- Public Key
@@ -27,9 +15,11 @@ main = do (message:exponent:modulus64:_) <- getArgs
           let cipherText = encrypt m e n
 
           print $ "-----BEGIN ENCRYPTED MESSAGE-----"
-          print $ encode (toByteString (cipherText :: Integer))
+          --print $ encode (toByteString (cipherText :: Integer))
+          print $ cipherText
           print $ "-----END ENCRYPTED MESSAGE-----"
--}
+
+
 --Encrypt m (plaintext) given the public exponent e and modulus n
 encrypt :: Integer -> Integer -> Integer -> Integer
 encrypt m e n = (m^e) `mod` n
