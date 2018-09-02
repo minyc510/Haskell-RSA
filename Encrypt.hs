7710ac93f7f5785e0341584d297ba830da5abbf1
minyc510 <minyc510@gmail.com>
2018-09-02 14:58:50 -0700

    KeyChain selection in Encrypt.

--- Encrypt.hs
+++ Encrypt.hs
@@ -1,19 +1,18 @@
 --To compile this file: `ghc -o enc Encrypt.hs`
-import System.Environment
-import ModularOperations
+import System.Environment             -- Used for `getArgs`
 import Base64Conversions as B64
 
 main :: IO()
 main = do (message:exponent:modulus:_) <- getArgs
           --Parse Inputs
           let m = read message
-          let e = 65537
+          let e = 65537                     -- Fermat Number F_4: 2^(2n) + 1
           let n = B64.toInt modulus
 
           let cipherText = encrypt m e n
 
           putStrLn $ "-----BEGIN ENCRYPTED MESSAGE-----"
-          putStrLn $ B64.fromInt cipherText
+          putStrLn $ B64.fromInt cipherText -- Converts FROM an INT to Base64
           putStrLn $ "-----END ENCRYPTED MESSAGE-----"
 
 --Encrypt m (plaintext) given the public exponent e and modulus n
